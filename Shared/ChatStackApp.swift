1b8f33884e63d9a2902a5e15871aeae0d328ad79
HackingHackers <52874216+HackingHackers@users.noreply.github.com>
2021-05-21 22:21:07 +0800

    Initial Commit

---
+++ Shared/ChatStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatStackApp.swift
+//  Shared
+//
+//  Created by Andy on 2021/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
